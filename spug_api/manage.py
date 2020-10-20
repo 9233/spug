1d03f8f6e80260e4b674cfae8258276355931115
runoob <test@runoob.com>
2020-10-20 00:00:00 -0700

    123

--- spug_api/manage.py
+++ spug_api/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#test for tar
 # Copyright: (c) OpenSpug Organization. https://github.com/openspug/spug
 # Copyright: (c) <spug.dev@gmail.com>
 # Released under the AGPL-3.0 License.
